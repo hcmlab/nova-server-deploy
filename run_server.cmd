bdcdf8215dd8476f7fa46e5b3851b8935ae3df31
Tobias Baur <tobias.baur@uni-a.de>
2023-03-20 09:21:34 +0100

    added whisper transcript

--- run_server.cmd
+++ run_server.cmd
@@ -1,3 +1,3 @@
-.\venv\Scripts\activate.bat & python .\nova_server\nova_backend.py --host xxx.xxx.xxx.xx --port 27017 --cml_dir "X:\nova\cml" --data_dir "X:\nova\data"
+.\venv\Scripts\activate.bat & python .\nova_server\nova_backend.py --host xxx.xxx.xxx.xx --port 27017 --cml_dir "X:\nova\scripts" --data_dir "X:\nova\data"
 .\venv\Scripts\deactivate.bat
 pause
